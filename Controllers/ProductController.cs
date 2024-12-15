9ea20c39bc935e9d322947459fdbd34686eeb199
Ali Sleiman <asleiman@acksession.com>
2024-12-15 12:32:05 +0200

    Architecture done

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,46 @@
+﻿using ClothingStore.Services.DTOs;
+using ClothingStore.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clothing_Store_C_.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private IProductService _ProductService;
+
+        public ProductController(IProductService productService)
+        {
+            _ProductService = productService;
+;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            return Ok(await _ProductService.GetAllProductsAsync());
+        }
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetProductById(int id)
+        {
+            return Ok(await _ProductService.GetProductByIdAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddProductAsync([FromBody] AddProductDTO productDTO)
+        {
+            return Ok(await _ProductService.AddProductAsync(productDTO));
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateProduct([FromBody] AddProductDTO productDTO)
+        {
+            return Ok(await _ProductService.UpdateProductAsync(productDTO));
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProductAsync(int id)
+        {
+            return Ok(await _ProductService.DeleteProductAsync(id));
+        }
+
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using ClothingStore.Services.DTOs;
+using ClothingStore.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clothing_Store_C_.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetUsers() { 
+            return Ok(await _userService.GetAllUsersAsync());        
+        }
+        [HttpGet("{id:int}")]
+        public async Task<IActionResult> GetUserById(int id)
+        {
+            return Ok(await _userService.GetUserAsync(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddUser([FromBody] AddUserDTO userDTO)
+        {
+            return Ok(await _userService.CreateUserAsync(userDTO));
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateUser([FromBody] AddUserDTO userDTO)
+        {
+            return Ok(await _userService.UpdateUserAsync(userDTO));
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteUser(int id)
+        {
+            return Ok(await _userService.DeleteUserAsync(id));
+        }
+
+    }
+}
